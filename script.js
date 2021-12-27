203b49474b42870805f3987eac71c1bc4c298a3a
luiscarriel <vitorluiiscarriel@gmail.com>
2021-12-27 13:48:14 -0500

    Atualização

---
+++ script.js
@@ -0,0 +1,9 @@
+var n1 = Number((prompt ("Digite um número")))
+var n2 = Number ((prompt ("Digite outro número")))
+var confirme = (prompt ("Você tem certeza?"))
+
+
+var soma = n1 * n2;
+
+
+alert(soma);
