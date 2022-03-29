21fcd4e948b65a8785a1f1dae5f09007616a0068
luiscarriel <vitorluiiscarriel@gmail.com>
2022-03-29 10:33:23 -0500

    feat: button function

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-var n1 = Number((prompt ("Digite um número")))
-var n2 = Number ((prompt ("Digite outro número")))
-var confirme = (prompt ("Você tem certeza?"))
+// adição
+var result = document.getElementById("result")
+
+function adicao() {
+    var n1 = Number((prompt("Digite um número")))
+    var n2 = Number((prompt("Digite outro número")))
+    var somar = n1 + n2;
+
+    result.innerHTML = ` Resultado da adição: ${somar}`
+}
+
+// subtração
+
+var result = document.getElementById("result")
+
+function subtracao() {
+    var n3 = Number((prompt("Digite um número")))
+    var n4 = Number((prompt("Digite outro número")))
+    var subtrair = n3 - n4;
+
+    result.innerHTML = ` Resultado da subtração:${subtrair}`
+}
+
+// multiplicação
+var result = document.getElementById("result")
+
+function multiplicacao() {
+    var n5 = Number((prompt("Digite um número")))
+    var n6 = Number((prompt("Digite outro número")))
+    var multiplicacao = n5 * n6;
+
+    result.innerHTML = ` Resultado da multiplicação:${multiplicacao}`
+}
+
+// divisão
+var result = document.getElementById("result")
+
+function divisao() {
+    var n7 = Number((prompt("Digite um número")))
+    var n8 = Number((prompt("Digite outro número")))
+    var divisao = n7 / n8;
+
+    result.innerHTML = ` Resultado da divisão:${divisao}`
+}
 
 
-var soma = n1 * n2;
 
 
-alert(soma);
+
+
